19a3655aa89d8cd32d15d8b2725c6e8f927c3c55
fmidory@gmail.com <midory@nbf-cps31.lab.dextra.com.br>
2015-11-04 08:25:49 -0200

    Copying matheus' repository

---
+++ arquivos/backup_base.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Variaveis de ambiente
+
+MASTER=postgresql02
+PGUSER=repl
+PORT=5432
+DBNAME=postgres
+BINDIR=/usr/local/pgsql/bin
+PGDATA=/dados/postgresql
+
+$BINDIR/pg_ctl -D $PGDATA stop -mi > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "PostgreSQL ja esta parado."
+fi
+
+# Backup
+
+rm -rf $PGDATA
+
+$BINDIR/pg_basebackup -D $PGDATA -R -P -h $MASTER -U $PGUSER
+
+# Restauracao
+
+sed -i 's/#hot_standby = off/hot_standby = on/g' $PGDATA/../config/replicacao.conf
+
+$BINDIR/pg_ctl -D $PGDATA start
+
+if [ $? -eq 0 ]; then
+  echo "Backup finalizado com sucesso!"
+fi
